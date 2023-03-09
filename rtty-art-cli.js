acbda0850f0e5ae1d66062ff9f435b90d19b13e4
Steven Lawson <slawson@zipcar.com>
2023-03-09 17:15:52 -0600

    WIP: convert to vanilla js, image cropping now working

--- rtty-art-cli.js
+++ rtty-art-cli.js
@@ -9,27 +9,37 @@ const height = 47;
 const currentSection = new PNG({ width, height });
 const currentDiff = new PNG({ width, height });
 
-image1.bitblt(currentSection, 0, 0, width, height, 0, 0);
+program.option("-p, --path <string>", "path to png file");
+program.parse();
+const options = program.opts();
+
+console.log(options.path);
+
+let dst = new PNG({ width: 30, height: 47 });
+fs.createReadStream(options.path)
+  .pipe(new PNG())
+  .on("parsed", function () {
+    try {
+      this.bitblt(dst, 250, 250, 30, 47, 0, 0);
+      dst.pack().pipe(fs.createWriteStream("out.png"));
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+/*image1.bitblt(currentSection, 0, 0, width, height, 0, 0);
 fs.writeFileSync("diff.png", PNG.sync.write(currentSection));
 
 pixelMatch(currentSection.data, image2.data, currentDiff.data, width, height, {
   threshold: 0.1,
   diffMask: true,
-});
+});*/
 
-let pixelDiffCount = 0;
+/*let pixelDiffCount = 0;
 for (let y = 0; y < currentDiff.height; y++) {
   for (let x = 0; x < currentDiff.width; x++) {
     let idx = (currentDiff.width * y + x) << 2;
     pixelDiffCount += currentDiff.data[idx + 4] === 0 ? 0 : 1; // index 4 is opacity
   }
 }
-console.log(pixelDiffCount);
-
-/*
-program.option("--first").option("-s, separator <char>");
-program.parse();
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
-console.log(program.args[0].split(options.separator, limit));
-*/
+console.log(pixelDiffCount);*/
